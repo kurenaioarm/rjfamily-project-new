bd64dcbb9e310f507631072357c1298cc75a0419
ShadoWoARM <kurenaioarm@gmail.com>
2023-03-23 15:43:58 +0700

    .

---
+++ web/doc_sched/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ttskch/select2-bootstrap4-theme' => array(
+            'pretty_version' => '1.5.2',
+            'version' => '1.5.2.0',
+            'reference' => '03fe405b21e4f8165a617b2b269346b1ffdc3619',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ttskch/select2-bootstrap4-theme',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
